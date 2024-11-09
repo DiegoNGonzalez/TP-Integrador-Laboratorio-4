b2d9a06efbd3be747aee613ab4099d8bb103035b
Diego Gonzalez <diego.gonzalez2@alumnos.frgp.utn.edu.ar>
2024-11-09 16:24:36 -0300

    interface cliente dao con metodos

--- TPINT_GRUPO_2_LAB4/src/dao/ClienteDao.java
+++ TPINT_GRUPO_2_LAB4/src/dao/ClienteDao.java
@@ -1,5 +1,13 @@
 package dao;
 
+import java.util.ArrayList;
+
+import entidades.Cliente;
+
 public interface ClienteDao {
 
+	public boolean agregarCliente(Cliente cliente);
+	public ArrayList<Cliente> listarClientes();
+	public void modificarCliente(int idCliente);
+	public boolean bajaCliente(int idCliente);
 }

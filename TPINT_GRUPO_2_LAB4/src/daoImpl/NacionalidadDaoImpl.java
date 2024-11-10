ded26ab928b8b0de76025857b7a1e34c1586b884
thecloudbuster <francocataldo7@gmail.com>
2024-11-10 19:42:39 -0300

    LocalidadDao, NacionalidadDao, ProvinciaDao

--- TPINT_GRUPO_2_LAB4/src/daoImpl/NacionalidadDaoImpl.java
+++ TPINT_GRUPO_2_LAB4/src/daoImpl/NacionalidadDaoImpl.java
@@ -1,13 +1,35 @@
 package daoImpl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import dao.NacionalidadDao;
+import entidades.Localidad;
 import entidades.Nacionalidad;
 
 public class NacionalidadDaoImpl implements NacionalidadDao {
+	@Override
 	public ArrayList<Nacionalidad> listarNacionalidades(){
+	    String query = "SELECT idNacionalidad, nacionalidad FROM nacionalidades";
 		ArrayList<Nacionalidad> listaNacionalidades = new ArrayList<>();	
+		
+	    try (Connection conexion = Conexion.getConnection();
+		         PreparedStatement statement = conexion.prepareStatement(query);
+		         ResultSet resultSet = statement.executeQuery()) {
+
+		        while (resultSet.next()) {
+		            Nacionalidad nacionalidad = new Nacionalidad();
+		            
+		            nacionalidad.setId(resultSet.getInt("idNacionalidad"));
+		            nacionalidad.setNacionalidad(resultSet.getString("nacionalidad"));
+		            listaNacionalidades.add(nacionalidad);
+		        }
+		    } catch (Exception e) {
+		        e.printStackTrace();
+		    }
+	    
 		return listaNacionalidades;
 	}
-}
+	}
--- TPINT_GRUPO_2_LAB4/src/daoImpl/ProvinciaDaoImpl.java
+++ TPINT_GRUPO_2_LAB4/src/daoImpl/ProvinciaDaoImpl.java
@@ -1,13 +1,36 @@
 package daoImpl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import dao.ProvinciaDao;
+import entidades.Nacionalidad;
 import entidades.Provincia;
 
 public class ProvinciaDaoImpl implements ProvinciaDao {
+	
+	@Override
 	public ArrayList<Provincia> listarProvincias(){
+	    String query = "SELECT idProvincia, provincia FROM provincias";
 		ArrayList<Provincia> listaProvincias = new ArrayList<>();	
+		
+	    try (Connection conexion = Conexion.getConnection();
+		         PreparedStatement statement = conexion.prepareStatement(query);
+		         ResultSet resultSet = statement.executeQuery()) {
+
+		        while (resultSet.next()) {
+		            Provincia provincia = new Provincia();
+		            
+		            provincia.setId(resultSet.getInt("idProvincia"));
+		            provincia.setProvincia(resultSet.getString("provincia"));
+		            listaProvincias.add(provincia);
+		        }
+		    } catch (Exception e) {
+		        e.printStackTrace();
+		    }
+	    
 		return listaProvincias;
 	}
 }
